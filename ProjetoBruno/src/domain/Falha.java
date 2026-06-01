bcc528a104dfd5592527c9c35fb052db03009ccc
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2026-06-01 18:56:40 -0300

    corrigindo implementações da classe dos observers

--- ProjetoBruno/src/domain/Falha.java
+++ ProjetoBruno/src/domain/Falha.java
@@ -24,8 +24,6 @@ public class Falha {
     private Prioridade prioridade;
     private final List<ObservadorFalha> observadores = new ArrayList<>();
 
-
-
     /**
      * 
      * @param dados Obtem a descrição e o tipo que está no objeto obtido
