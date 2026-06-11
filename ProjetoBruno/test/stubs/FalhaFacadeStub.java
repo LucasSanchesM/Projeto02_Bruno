6c02eddbe90d6607fa977f8754c9b81a2dce36c7
Mirell4 <mirellaviana21@icloud.com>
2026-06-11 00:37:58 -0300

    Documentação Facade Stubs Atualizada

--- ProjetoBruno/test/stubs/FalhaFacadeStub.java
+++ ProjetoBruno/test/stubs/FalhaFacadeStub.java
@@ -13,40 +13,60 @@ import state.EstadoEncerrado;
 
 
 /**
- *
+ *Classe herdada de Falha, responsável por lidar com o Facade
  * @author Mirel
  */
 public class FalhaFacadeStub extends Falha{
     
     private EstadoFalha estadoAtual;
 
+    /**
+    Cria uma instância do stub utilizando os dados informados.
+    @param dados dados utilizados na criação da falha.
+    */
     public FalhaFacadeStub(DadosFalha dados) {
-        super(dados);
+    super(dados);
     }
 
+    /**
+    Simula a transição para o estado EM_ANALISE.
+    */
     @Override
     public void iniciarAnalise() {
-        this.estadoAtual = new EstadoEmAnalise();
+    this.estadoAtual = new EstadoEmAnalise();
     }
 
+    /**
+    Simula a transição para o estado APROVADO.
+    */
     @Override
     public void aprovar() {
-        this.estadoAtual = new EstadoAprovado();
+    this.estadoAtual = new EstadoAprovado();
     }
 
+    /**
+    Simula a transição para o estado EM_ATENDIMENTO.
+    */
     @Override
     public void iniciarAtendimento() {
-        this.estadoAtual = new EstadoEmAtendimento();
+    this.estadoAtual = new EstadoEmAtendimento();
     }
 
+    /**
+    Simula a transição para o estado ENCERRADO.
+    */
     @Override
     public void encerrar() {
-        this.estadoAtual = new EstadoEncerrado();
+    this.estadoAtual = new EstadoEncerrado();
     }
 
+    /**
+    Retorna o estado atual armazenado pelo stub.
+    @return estado atual da falha.
+    */
     @Override
     public EstadoFalha getEstadoAtual() {
-        return estadoAtual;
+    return estadoAtual;
     }
     
 }
